d6b81d295e177c93cca052355c8144313d346db7
Anas Khan <anas89_se@hotmail.com>
2020-10-23 20:04:17 +0500

    add build number

--- resources/com/griddynamics/devops/mpl/modules/Deploy/Deploy.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Deploy/Deploy.groovy
@@ -3,7 +3,7 @@
  */
 
 //MPLModule('Openshift Deploy', CFG)
-withEnv(['nexus_url=10.0.2.2']) {
+withEnv(['nexus_url=10.0.2.2','build_version =env.BUILD_NUMBER']) {
     echo 'nexus url = ${nexus_url}'
     sh 'mvn deploy -DskipTests -Dnexus_url=${nexus_url}'
 }
