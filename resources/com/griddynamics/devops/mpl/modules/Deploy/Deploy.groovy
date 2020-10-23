8128c84df2064bfd4b206ae6e2ecece81e8ca37f
Anas Khan <anas89_se@hotmail.com>
2020-10-23 20:17:34 +0500

    update artifact version

--- resources/com/griddynamics/devops/mpl/modules/Deploy/Deploy.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Deploy/Deploy.groovy
@@ -3,8 +3,8 @@
  */
 
 //MPLModule('Openshift Deploy', CFG)
-withEnv(['nexus_url=10.0.2.2',"build_version = ${BUILD_NUMBER}"]) {
+withEnv(['nexus_url=10.0.2.2',"build_version=${BUILD_NUMBER}"]) {
     echo 'build number= ${build_version}'
-    echo 'nexus url = ${nexus_url}'
-    sh 'mvn deploy -DskipTests -Dnexus_url=${nexus_url}'
+    echo "nexus url = ${nexus_url}"
+    sh 'mvn deploy -DskipTests -Dnexus_url=${nexus_url} -Dbuild_version=$build_version'
 }
